42fd922a697040cf6f0c5b4ddfee24e55534ff43
RicccardoVenturino <100801309+RicccardoVenturino@users.noreply.github.com>
2022-04-02 20:27:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float z = 0, a = 0, b = 0, x = 0, y = 0;
+    cout << "Inserire quattro valori reali:" << endl;
+    cin >> a >> b >> x >> y;
+
+    if((x<0 && y>0)) {
+        z = a*x - b*y;
+        cout << z << endl;
+    }else if((x>=0 && y<=0)) {
+        z = a*x*x - b*y;
+        cout << z << endl;
+    }else {
+        z = a*x + b*y*y;
+        cout << z << endl;
+    }
+    return 0;
 }
